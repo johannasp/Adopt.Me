d24fa4ee32deed5d48b0ac57e11607745a3efc48
Johanna Smith-Palliser <godinas@Johannas-MacBook-Pro.local>
2017-06-10 22:18:34 -0400

    Update database and results view

--- Adopt.Me/CollectionViewController.swift
+++ Adopt.Me/CollectionViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class CollectionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate{
+class CollectionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
     
     var favesTableView: UITableView!
     var favoriteAnimals = [Animal]()
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,9 +30,9 @@ class CollectionViewController: UIViewController, UITableViewDataSource, UITable
         view.addSubview(backButton)
         
         
-        favesTableView = UITableView(frame: CGRect(x: 0, y: 0, width: view.frame.width - 10, height: 250))
+        favesTableView = UITableView(frame: CGRect(x: 0, y: 0, width: view.frame.width - 10, height: 150))
         favesTableView.center.x = view.center.x
-        favesTableView.center.y = 300
+        favesTableView.frame.origin.y = backButton.frame.origin.y + backButton.frame.height + 20
         favesTableView.dataSource = self
         favesTableView.delegate = self
         favesTableView.tableFooterView = UIView()
@@ -41,6 +42,20 @@ class CollectionViewController: UIViewController, UITableViewDataSource, UITable
         favesTableView.layer.borderColor = UIColor(red: 181/255, green: 195/255, blue: 204/255, alpha: 1).cgColor
         favesTableView.layer.borderWidth = 1.0
         view.addSubview(favesTableView)
+        
+        
+        // collection view setup
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 7, bottom: 10, right: 7)
+        layout.itemSize = CGSize(width: 80, height: 100)
+        
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 300), collectionViewLayout: layout)
+        collectionView.frame.origin.y = favesTableView.frame.origin.y + favesTableView.frame.height + 30
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.backgroundColor = .red
+        self.view.addSubview(collectionView)
         
     }
     
@@ -60,17 +75,41 @@ class CollectionViewController: UIViewController, UITableViewDataSource, UITable
     
     override func viewWillAppear(_ animated: Bool) {
         favesTableView.reloadData()
+        collectionView.reloadData()
     }
     
     /*
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let group = favoriteAnimals[indexPath.row]
-        let interestsViewController = InterestsViewController()
-        interestsViewController.group = group
-        navigationController?.pushViewController(interestsViewController, animated: true)
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     
+     let group = favoriteAnimals[indexPath.row]
+     let interestsViewController = InterestsViewController()
+     interestsViewController.group = group
+     navigationController?.pushViewController(interestsViewController, animated: true)
+     }
+     */
+    
+    
+    
+    // more collection view setup
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //number of items in collection view
+        return favoriteAnimals.count
     }
-    */
-
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath as IndexPath)
+        
+        let nameLabel = UILabel(frame: CGRect(x: 0, y: 0, width: cell.frame.width, height: 30))
+        nameLabel.text = favoriteAnimals[indexPath.row].name
+        nameLabel.sizeToFit()
+        nameLabel.center.x = cell.center.x
+        nameLabel.frame.origin.y = cell.frame.height - nameLabel.frame.height
+        cell.addSubview(nameLabel)
+        
+        cell.backgroundColor = .orange
+        return cell
+    }
+    
     
 }
